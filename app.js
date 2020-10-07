ad6bb5aa8f6af47e3dd0d4ac54755b38c8c3fc17
steveb <steveb@motabilityoperations.co.uk>
2020-10-07 16:03:41 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const chalk = require("chalk");
+const { demandOption } = require("yargs");
+const yargs = require("yargs");
+const notes = require("./notes.js");
+
+//Customize yargs version
+yargs.version("1.1.0");
+
+//Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
+
+//Create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
+    console.log("Removing the note");
+  },
+});
+
+//Create List command
+yargs.command({
+  command: "list",
+  describe: "list the notes",
+  handler: function () {
+    console.log("Listing out all notes");
+  },
+});
+
+//Create Read command
+yargs.command({
+  command: "read",
+  describe: "reading a note",
+  handler: function () {
+    console.log("Reading a note");
+  },
+});
+
+yargs.parse();
+
+//add, remove, read, list
+//console.log(yargs.argv);
